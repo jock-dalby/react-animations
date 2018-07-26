b8ea5eee2f0e114956a5788fe9d7db23fc9b1de3
jock-dalby <jockdalby86@gmail.com>
2018-07-26 16:18:17 +1200

    install react-transition-group and implement an animation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Transition from 'react-transition-group/Transition';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -7,7 +8,8 @@ import List from "./components/List/List";
 
 class App extends Component {
   state = {
-    modelOpen: false
+    modelOpen: false,
+    showBlock: false
   }
 
   showModal = () => {
@@ -23,6 +25,21 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button className="Button" onClick={() => this.setState(prevState => ({showBlock: !prevState.showBlock}))}>Toggle</button>
+        <br />
+        {/* mountOnEnter unmountOnExit adds the Transition element to and from the dom as it is shown and hidden */}
+        <Transition in={this.state.showBlock} timeout={1000} mountOnEnter unmountOnExit>
+          {state => (
+            <div style={{
+              backgroundColor: 'red',
+              width: 100,
+              height: 100,
+              margin: 'auto',
+              transition: 'opacity 1s ease-out',
+              opacity: state === 'exited' ? 0 : 1
+              }}></div>
+          )}
+        </Transition>
         <Modal closed={this.hideModal} show={this.state.modelOpen}/>
         <Backdrop show={this.state.modelOpen}/>
         <button className="Button" onClick={this.showModal}>Open Modal</button>
